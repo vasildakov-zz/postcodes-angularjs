3fcb6175b47a6efee4374b467be63ea92f2dfa17
Vasil Dakov <vasildakov@gmail.com>
2016-11-01 11:35:49 +0000

    Updated postcodes service and unit tests

--- app/scripts/services/postcodes.js
+++ app/scripts/services/postcodes.js
@@ -9,29 +9,68 @@
  */
 app.factory('Postcodes',  ['$http', function($http) {
 
-	var Postcodes = {};
+    var Postcodes = {};
 
     Postcodes.get = function (postcode) {
         return [2, 4, 6, 8];
     }
 
+
+    Postcodes.query = function (postcode) {
+        //
+    }
+
+
+    Postcodes.lookup = function (postcode) {
+        return $http({
+            url: '/api/lookup',
+            method: "GET",
+            params: {postcode: postcode}
+        });
+    }
+
+    Postcodes.outward = function (outward) {
+        //
+    }
+
+
+    Postcodes.inward = function (inward) {
+        //
+    }
+
+
+    Postcodes.nearest = function (lat, lon) {
+        return $http({
+            url: '/api/nearest?latitude=' + lat + '&longitude=' + lon,
+            method: "GET"
+            //params: {latitude: latitude, longitude: longitude}
+        });
+    }
+
+
+    Postcodes.distance = function (from, to) {
+        //
+    }
+
+
+    Postcodes.random = function () {
+        //
+    }
+
     Postcodes.autocomplete = function (postcode) {
-        return $http.get('/api/autocomplete', {
-        	params: { postcode: postcode }
-    	});
-        
+        return $http.get('/api/autocomplete/' + postcode);
+
         /*
         return $http({
-		    url: '/api/autocomplete', 
-		    method: "GET",
-		    params: {postcode: postcode}
-		});
-		*/
+            url: '/api/autocomplete',
+            method: "GET",
+            params: {postcode: postcode}
+        });
+        */
     }
 
     Postcodes.validate = function(postcode) {
         //
-
     }
 
     return Postcodes;
--- test/spec/services/postcodes.js
+++ test/spec/services/postcodes.js
@@ -15,34 +15,56 @@ describe('Postcodes', function(){
 	});
 
 
-    describe('when I call Postcodes.get', function(){
-        it('returns an array with 4 elements', inject(function($httpBackend, Postcodes) {
+    describe('when I call postcodes get', function(){
+        it('returns an array with 4 elements', function() {
             expect(Postcodes.get().length).toBe(4);
-        }));
+        });
     });
 
 
-    describe('when I call Postcodes.autocomplete', function(){
-        it('returns something', inject(function($httpBackend, Postcodes) {
+    describe('when I call postcodes autocomplete with partial postcode', function(){
+        it('returns something', function() {
 
         	var postcode = 'TW8 8';
 
-        	$httpBackend
-        		.when('GET', '/api/autocomplete')
-        		.respond(200, [
-        			"TW8 8FA", "TW8 8B", "TW8 8B"
-        		]);
+            $httpBackend
+                .when('GET', '/api/autocomplete/' + postcode)
+                .respond(200, [
+                    "TW8 8FA", "TW8 8B", "TW8 8B"
+                ]);
 
-            Postcodes.autocomplete().then(function(response) {
-            	console.log(response);
+            Postcodes.autocomplete(postcode).then(function(response) {
+                //console.log(response);
                 expect(response.status).toEqual(200);
                 expect(response.data.length).toEqual(3);
             });
 
             $httpBackend.flush();
 
-        }));
+        });
+    });
 
+
+    describe('when I call postcodes nearest with latitude and longitude', function(){
+        it('returns something', function() {
+
+            var latitude  = 51.483954952877600,
+                longitude = -0.312577856018865;
+
+            $httpBackend
+                .when('GET', '/api/nearest?latitude=' + latitude + '&longitude=' + longitude)
+                .respond(200, [
+                    "TW8 1", "TW8 2", "TW8 3", "TW8 4", "TW8 5"
+                ]);
+
+            Postcodes.nearest(latitude, longitude).then(function(response) {
+                expect(response.status).toEqual(200);
+                expect(response.data.length).toEqual(5);
+            });
+
+            $httpBackend.flush();
+
+        });
     });
 
 });
