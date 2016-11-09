323b93f4139eeb8c0098d53fb2764b18e476d59e
Vasil Dakov <vasildakov@gmail.com>
2016-11-09 00:14:33 +0000

    WIP

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,12 @@
  */
 app.controller('MainCtrl', function ($scope, Postcodes) {
 
+    $scope.show = false;
+
     $scope.test = JSON.stringify({
-        "name":"John Johnson",
-        "street":"Oslo West 16",
-        "phone":"555 1234567"
+        "name": "John Johnson",
+        "street": "Oslo West 16",
+        "phone": "555 1234567"
     }), 4;
 
     $scope.postcode = null;
@@ -23,12 +25,23 @@ app.controller('MainCtrl', function ($scope, Postcodes) {
 
     $scope.lookup = function(postcode) {
         Postcodes.lookup(postcode).then(function(response){
-            $scope.response = response;
-            $scope.data = JSON.stringify(response.data, 4);
 
-            hljs.initHighlighting();
+            var str  = "";
+                str += "GET /postcodes/"+postcode+"\n"; 
+                str += "HTTP/1.1 Host: example.org\n";
+                str += "Content-Type: application/json; charset=utf-8\n";
+                str += "Content-Length: 124\n";
+                str += JSON.stringify(response.data, null, 4);
+            $scope.response = response;
+            //$scope.data = JSON.stringify(response.data, null, Number(4));
+            $scope.data = str;
+
         });
     };
+
+    /* $scope.$watch($scope.data, function(newVal, oldVal){
+        console.log('changed');
+    }, true);*/
 
     // $scope.postcode = JSON.stringify(response, null, 4)
     // console.log($scope.response);
